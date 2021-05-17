2ad520d67a6ac15111b73e60b77282b3a8eff668
WEIHAIRC <52539369+WEIHAIRC@users.noreply.github.com>
2021-05-16 18:22:32 -0700

    Update router.js

--- scripts/router.js
+++ scripts/router.js
@@ -10,13 +10,17 @@ var entrySelector= document.querySelector("entry-page");
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function(page, index, entry) {
+router.setState = function(page, index, entry, backward) {
     // page parameter is a string telling which page to load
 
     // Set to homepage, modify body
-    if (page == "home") {
+    // *** A huge bug here! page could be null when history is empty!
+    if (page == null || page == "home") {
         bodySelector.className = "index";
         titleSelector.innerHTML = "Journal Entries";
+        if (!backward) {
+            history.pushState(page, "home", '/');
+        }
 
         // follow 4.2 hint.
         let newPage = document.createElement("entry-page");
@@ -27,6 +31,10 @@ router.setState = function(page, index, entry) {
     if (page == "setting") {
         bodySelector.className = "settings";
         titleSelector.innerHTML = "Settings";
+        if (!backward) {
+            history.pushState(page,"settings", '#settings');
+        }
+        
         let newPage = document.createElement("entry-page");
         entrySelector.replaceWith(newPage);
     }
@@ -35,6 +43,9 @@ router.setState = function(page, index, entry) {
         bodySelector.className = "single-entry";
         entrySelector = document.querySelector("entry-page");
         titleSelector.innerHTML = `Entry ${index+1}`;
+        if (!backward) {
+            history.pushState(page, "entry", `#entry${index+1}`);
+        }
 
         let newPage = document.createElement("entry-page");
         // something wrong here, should be an entry, not a page.
