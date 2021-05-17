8c55be51d5ffb77cd20914341f911e26e5d9f9e5
WEIHAIRC <52539369+WEIHAIRC@users.noreply.github.com>
2021-05-16 20:43:26 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,67 +5,74 @@
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
 
+// sw.js - Service Worker
+
+// You will need 3 event listeners:
+//   - One for installation
+//   - One for activation ( check out MDN's clients.claim() for this step )
+//   - One for fetch requests
+
 
 // Code mostly from https://developers.google.com/web/fundamentals/primers/service-workers
 
-// Register a service worker, copied and pasted from Service Worker
+// Register a service worker, copied
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
     });
-  });
 }
 
-var CACHE_NAME = 'my-site-cache-v1';
+// Install a service worker
+var CACHE_NAME = 'Lab7 Cache';
 var urlsToCache = [
+  // simply put all related files here I guess?
   '/',
-  '/styles/main.css',
-  '/script/main.js'
+  '/scripts/router.js',
+  '/scripts/script.js',
+  '/components/entry-page.js',
+  '/components/journal-entry.js',
+  '/style.css',
+  '/index.html',
+  // Oh god, writeup says "use your service worker to cache and return requests from"
+  // this website but I forgot to put it here.
+  'https://cse110lab6.herokuapp.com/entries'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
 });
 
+// Update Service Worker, copied
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+
+// Cache and return requests, do we need that more complex one?
+// Try, answer is no. Copy the simpler one.
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
           }
+  
+          return fetch(event.request);
         })
       );
-    })
-  );
 });
